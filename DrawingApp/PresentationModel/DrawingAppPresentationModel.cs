69aaecc77095c3f259bc32b2681d1eb2aa076ae9
zaqNitori <zaq81009@gmail.com>
2021-12-04 20:36:01 +0800

    完成 Window APP 畫面 ， 包含按鈕狀態改變。

--- DrawingApp/PresentationModel/DrawingAppPresentationModel.cs
+++ DrawingApp/PresentationModel/DrawingAppPresentationModel.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using DrawingModel;
+using DrawingModel.Shape;
 
 namespace DrawingApp.PresentationModel
 {
     class DrawingAppPresentationModel
     {
+        public event ModelChangedEventHandler _modelChanged;
+        public delegate void ModelChangedEventHandler();
         DrawModel _model;
         IGraphics _igraphics;
 
@@ -17,15 +20,50 @@ namespace DrawingApp.PresentationModel
         {
             this._model = model;
             _igraphics = new DrawingAppGraphicsAdaptor(canvas);
+            _model._modelChanged += NotifyModelChanged;
+            IsButtonClearEnabled = IsButtonDrawEllipseEnabled = IsButtonDrawRectangleEnabled = true;
         }
 
-        /// <summary>
-        /// 繪製
-        /// </summary>
+        public bool IsButtonClearEnabled
+        {
+            get; set;
+        }
+
+        public bool IsButtonDrawRectangleEnabled
+        {
+            get; set;
+        }
+
+        public bool IsButtonDrawEllipseEnabled
+        {
+            get; set;
+        }
+
+        // 繪製
         public void Draw()
         {
             // 重複使用igraphics物件
             _model.Draw(_igraphics);
         }
+
+        // 清除畫布
+        public void Clear()
+        {
+            _model.Clear();
+        }
+
+        // 設定 要繪製的圖形
+        public void DrawShpaeType(ShapeType shapeType)
+        {
+            //do nothing
+        }
+
+        // 同步通知
+        void NotifyModelChanged()
+        {
+            if (_modelChanged != null)
+                _modelChanged();
+        }
+
     }
 }
